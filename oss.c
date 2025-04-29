13c1ed8f0fe02a99636819ff05d74e3226416fb9
SwatiShah2000 <swastikashah2000@gmail.com>
2025-04-29 00:10:52 -0500

    oss.c  commit

--- oss.c
+++ oss.c
@@ -659,3 +659,11 @@ int compareTime(unsigned int sec1, unsigned int ns1, unsigned int sec2, unsigned
     if (ns1 > ns2) return 1;
     return 0;
 }
+
+
+// Get the elapsed time from start to end
+void getElapsedTime(unsigned int start_sec, unsigned int start_ns,
+                   unsigned int end_sec, unsigned int end_ns,
+                   unsigned int *elapsed_sec, unsigned int *elapsed_ns) {
+    *elapsed_sec = end_sec - start_sec;
+    if (end_ns < start_ns) {
